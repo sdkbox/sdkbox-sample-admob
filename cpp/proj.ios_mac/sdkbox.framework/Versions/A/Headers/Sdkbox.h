d4f2a21f8e9e6c5ce366732b885cbf223d39219f
yinjimmy <jimmy.yin5@gmail.com>
2018-05-07 10:10:18 +0800

    2.4.0.0

--- cpp/proj.ios_mac/sdkbox.framework/Versions/A/Headers/Sdkbox.h
+++ cpp/proj.ios_mac/sdkbox.framework/Versions/A/Headers/Sdkbox.h
@@ -7,7 +7,7 @@
 #ifndef _CC_SDKBOX_H_
 #define _CC_SDKBOX_H_
 
-#define SDKBOX_VERSION_STR  "sdkbox V2.3.17.8"
+#define SDKBOX_VERSION_STR  "sdkbox V2.4.0.0"
 
 /**
  * Only certain compilers support __attribute__((deprecated)).
